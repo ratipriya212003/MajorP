e95f33060f12073da48ed1486fe6929aeac253d4
Ratipriya <133866565+ratipriya212003@users.noreply.github.com>
2024-07-24 02:06:52 +0530

    Update StoreContext.js

--- rentalfront/src/Context/StoreContext.js
+++ rentalfront/src/Context/StoreContext.js
@@ -8,7 +8,7 @@ export function StoreContextProvider(props) {
   const [rental_list,setRentalList] =useState([]);
   const [owners,setOwners]=useState([]);
 
-const url="https://easydrive-ug3p.onrender.com";
+const url="https://easydrivebackend.onrender.com";
 
 
   const addToCart = (itemId) => {
