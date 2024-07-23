6c0a5ce1419e722b4c7370addff85c3024c2c516
Ratipriya <133866565+ratipriya212003@users.noreply.github.com>
2024-07-24 00:54:07 +0530

    Update Login.js

--- rentalfront/src/components/Login.js
+++ rentalfront/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "../styles/Login.css";
 import {useNavigate} from "react-router-dom";
 import { StoreContext } from '../Context/StoreContext';
