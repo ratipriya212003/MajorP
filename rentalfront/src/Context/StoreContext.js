4a61dacb0318c5b9cc3d6ec1036cf65890c01e09
Ratipriya <133866565+ratipriya212003@users.noreply.github.com>
2024-07-24 17:24:55 +0530

    Update StoreContext.js

--- rentalfront/src/Context/StoreContext.js
+++ rentalfront/src/Context/StoreContext.js
@@ -8,7 +8,7 @@ export function StoreContextProvider(props) {
   const [rental_list,setRentalList] =useState([]);
   const [owners,setOwners]=useState([]);
 
-const url="https://easydrive212003.vercel.app/";
+const url="https://backend-three-sable.vercel.app/";
 
 
   const addToCart = (itemId) => {
@@ -39,7 +39,7 @@ const url="https://easydrive212003.vercel.app/";
 
   const fetchRentalList=async()=>{
      try {
-      const response = await fetch('https://easydrive212003.vercel.app/api/Rental/products', {
+      const response = await fetch('https://backend-three-sable.vercel.app//api/Rental/products', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -59,7 +59,7 @@ const url="https://easydrive212003.vercel.app/";
   
   const fetchOwnerList=async()=>{
      try {
-      const response = await fetch('https://easydrive212003.vercel.app/api/Rental/owners', {
+      const response = await fetch('https://backend-three-sable.vercel.app//api/Rental/owners', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
