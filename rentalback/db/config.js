b7ad597b265cc1fbefec852f99131bb1082dfe65
Ratipriya <133866565+ratipriya212003@users.noreply.github.com>
2024-07-31 01:25:21 +0530

    Update config.js

--- rentalback/db/config.js
+++ rentalback/db/config.js
@@ -1,2 +1,2 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/Rental");
+mongoose.connect("mongodb+srv://ratipriya212003:easydrive212003@cluster0.ranb5ae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
